2f0223afc89571a07194ffe8e2df6cc4178e9ada
KubiGR <adranidisb@gmail.com>
2023-04-15 01:19:53 +0300

    Add targetWhitelist and targetBlacklist

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var targetBlacklist = parseEnvList(process.env.CORSANYWHERE_TARGET_BLACKLIST);
+var targetWhitelist = parseEnvList(process.env.CORSANYWHERE_TARGET_WHITELIST);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -23,6 +25,8 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
+  targetBlacklist: targetBlacklist,
+  targetWhitelist: targetWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
